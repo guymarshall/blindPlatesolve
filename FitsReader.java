34546154a3bc6c2d9b23784f4506032d1fc7fc33
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2026-04-09 18:56:51 +0100

    WIP: getImage() performance optimisation

--- FitsReader.java
+++ FitsReader.java
@@ -1,10 +1,8 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
 
 public class FitsReader {
     public static Image getImage(String path) throws IOException {
-        List<Map.Entry<String, String>> headers = new ArrayList<>();
         boolean endReached = false;
         int width = 0;
         int height = 0;
@@ -13,8 +11,8 @@ public class FitsReader {
             byte[] block = new byte[2880];
 
             while (!endReached) {
-                int read = reader.readNBytes(block, 0, 2880);
-                if (read < 2880) {
+                int bytesRead = reader.readNBytes(block, 0, 2880);
+                if (bytesRead < 2880) {
                     throw new EOFException("Unexpected end of file");
                 }
 
@@ -28,29 +26,30 @@ public class FitsReader {
                         break;
                     }
 
-                    int equalPosition = line.indexOf('=');
-                    if (equalPosition != -1) {
-                        String value = line.substring(equalPosition + 1);
+                    if (keyword.equals("NAXIS1") || keyword.equals("NAXIS2")) {
+                        int equalPosition = line.indexOf('=');
+                        if (equalPosition != -1) {
+                            String value = line.substring(equalPosition + 1);
 
-                        int slashPosition = value.indexOf('/');
-                        if (slashPosition != -1) {
-                            value = value.substring(0, slashPosition);
-                        }
+                            int slashPosition = value.indexOf('/');
+                            if (slashPosition != -1) {
+                                value = value.substring(0, slashPosition);
+                            }
 
-                        value = value.trim();
-                        headers.add(new AbstractMap.SimpleEntry<>(keyword, value));
+                            value = value.trim();
 
-                        if (keyword.equals("NAXIS1")) {
-                            width = Integer.parseInt(value);
-                        }
+                            if (keyword.equals("NAXIS1")) {
+                                width = Integer.parseInt(value);
+                            }
 
-                        if (keyword.equals("NAXIS2")) {
-                            height = Integer.parseInt(value);
-                        }
+                            if (keyword.equals("NAXIS2")) {
+                                height = Integer.parseInt(value);
+                            }
 
-                        if (width != 0 && height != 0) {
-                            endReached = true;
-                            break;
+                            if (width != 0 && height != 0) {
+                                endReached = true;
+                                break;
+                            }
                         }
                     }
                 }
